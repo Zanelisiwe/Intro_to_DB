9f204a9145949ebe78b175d5420f14e70ad3658e
Zanelisiwe <ntombelazanelisiwe@outlook.com>
2025-08-17 20:24:17 +0200

    add ""except mysql.connector.Error""

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,5 @@
 # MySQLServer.py
 import mysql.connector
-from mysql.connector import Error
 
 try:
     # Connect to MySQL Server
@@ -9,16 +8,16 @@ try:
         user="root",
         password="your_password"
     )
-    
+
     if connection.is_connected():
         cursor = connection.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error while connecting to MySQL: {e}")
 
 finally:
-    if connection.is_connected():
+    if 'connection' in locals() and connection.is_connected():
         cursor.close()
         connection.close()
